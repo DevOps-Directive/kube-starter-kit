0795be62b6a6d81d61064138d103751dc93d2023
“Sid <sid@devopsdirective.com>
2025-10-02 12:58:59 -0400

    add eks + kapenter modules

--- terraform/live/aws/staging/us-east-2/eks/main.tf
+++ terraform/live/aws/staging/us-east-2/eks/main.tf
@@ -21,8 +21,88 @@ provider "aws" {
   }
 }
 
-resource "null_resource" "default" {
-  provisioner "local-exec" {
-    command = "echo 'Hello World'"
+data "aws_availability_zones" "available" {
+  # Exclude local zones
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
   }
 }
+
+
+
+locals {
+  name               = "staging-us-east-2"
+  kubernetes_version = "1.33"
+  region             = "us-east-2"
+
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "21.3.1"
+
+  name               = local.name
+  kubernetes_version = "1.33"
+
+  # Gives Terraform identity admin access to cluster which will
+  # allow deploying resources (Karpenter) into the cluster
+  enable_cluster_creator_admin_permissions = true
+  endpoint_public_access                   = true
+
+  # TODO: specify versions
+  addons = {
+    coredns = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      before_compute = true
+    }
+  }
+
+  vpc_id     = "vpc-0955989470c913b10"
+  subnet_ids = ["subnet-010a63eb87d2020b1", "subnet-047960b613aba7131", "subnet-01e2b29b02d52e90f"]
+
+  eks_managed_node_groups = {
+    karpenter = {
+      ami_type       = "BOTTLEROCKET_x86_64"
+      instance_types = ["t3.small"]
+
+      min_size     = 2
+      max_size     = 3
+      desired_size = 2
+
+      labels = {
+        # Used to ensure Karpenter runs on nodes that it does not manage
+        "karpenter.sh/controller" = "true"
+      }
+    }
+  }
+
+  node_security_group_tags = {
+    "karpenter.sh/discovery" = local.name
+  }
+
+}
+
+module "karpenter" {
+  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version = "21.3.1"
+
+  cluster_name = module.eks.cluster_name
+
+  # Name needs to match role name passed to the EC2NodeClass
+  node_iam_role_use_name_prefix   = false
+  node_iam_role_name              = local.name
+  create_pod_identity_association = true
+
+  # Used to attach additional IAM policies to the Karpenter node IAM role
+  node_iam_role_additional_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+}
+
