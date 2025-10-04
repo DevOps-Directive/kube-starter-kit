594679aecf32e6f2ea608d3ae4532328a8022adf
“Sid <sid@devopsdirective.com>
2025-10-04 14:22:20 -0400

    comment out deploy key

--- terraform/live/github/kube-starter-kit/main.tf
+++ terraform/live/github/kube-starter-kit/main.tf
@@ -15,19 +15,18 @@ terraform {
 }
 
 provider "github" {
-  # Configuration options
   owner = "DevOps-Directive"
 }
 
-# Generate an ssh key using provider "hashicorp/tls"
 resource "tls_private_key" "deploy_key" {
   algorithm = "ED25519"
 }
 
-# Add the ssh key as a deploy key
-resource "github_repository_deploy_key" "example_repository_deploy_key" {
-  title      = "Repository test key"
-  repository = "kube-starter-kit"
-  key        = tls_private_key.deploy_key.public_key_openssh
-  read_only  = true
-}
+# TODO: This can't be deployed with standard github action token permissions...
+#       Need to figure out a way to automate (PAT or octo-sts)
+# resource "github_repository_deploy_key" "example_repository_deploy_key" {
+#   title      = "Repository test key"
+#   repository = "kube-starter-kit"
+#   key        = tls_private_key.deploy_key.public_key_openssh
+#   read_only  = true
+# }
