e9a82d6345cb566702a170b7ac362d8dfec56d40
“Sid <sid@devopsdirective.com>
2025-10-05 21:14:46 -0400

    add association for external secrets IAM

--- terraform/live/aws/staging/us-east-2/eks/base-infra-resources.tf
+++ terraform/live/aws/staging/us-east-2/eks/base-infra-resources.tf
@@ -11,6 +11,11 @@ module "external_secrets_pod_identity" {
   external_secrets_secrets_manager_arns = ["arn:aws:secretsmanager:*:*:secret:*"]
   external_secrets_create_permission    = true # Necessary to use https://external-secrets.io/latest/api/pushsecret/
 
+  association_defaults = {
+    cluster_name    = module.eks.cluster_name
+    namespace       = "external-secrets"
+    service_account = "external-secrets"
+  }
 }
 
 module "aws_ebs_csi_pod_identity" {
@@ -20,4 +25,9 @@ module "aws_ebs_csi_pod_identity" {
 
   attach_aws_ebs_csi_policy = true
 
+  # associations = {
+  #   cluster_name    = module.eks.cluster_name
+  #   namespace       = "external-secrets"
+  #   service_account = "external-secrets"
+  # }
 }
