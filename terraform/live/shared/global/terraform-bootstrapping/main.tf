ce5a91b8ab4744a7ddc84f7243de006f788e96e2
sidpalas <1320389+sidpalas@users.noreply.github.com>
2025-10-11 13:54:25 -0400

    chore: refactor tf structure to be env centric (#14)

---
+++ terraform/live/shared/global/terraform-bootstrapping/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  backend "s3" {
+    bucket       = "kube-starter-kit-tf-state"
+    key          = "shared/global/terraform-bootstrapping.tfstate"
+    region       = "us-east-2"
+    use_lockfile = "true"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region              = "us-east-2"
+  allowed_account_ids = ["094905625236"]
+}
+
+module "github-oidc-provider" {
+  source  = "terraform-module/github-oidc-provider/aws"
+  version = "2.2.1"
+
+  create_oidc_provider = true
+  create_oidc_role     = true
+
+  repositories              = ["DevOps-Directive/kube-starter-kit"]
+  oidc_role_attach_policies = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+}
+
+module "state-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "5.7.0"
+
+  bucket = "kube-starter-kit-tf-state"
+
+  versioning = {
+    enabled = true
+  }
+}
+
+module "plans-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "5.7.0"
+
+  bucket = "kube-starter-kit-tf-digger-plans"
+
+  versioning = {
+    enabled = true
+  }
+}
+
