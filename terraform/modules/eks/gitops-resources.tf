3912208020153bfa05eb087e3a798269c5b6dc9b
“Sid <sid@devopsdirective.com>
2025-10-27 15:11:23 -0400

    remove digger flag, store PEM secret

--- terraform/modules/eks/gitops-resources.tf
+++ terraform/modules/eks/gitops-resources.tf
@@ -17,12 +17,16 @@ module "secrets_manager_json" {
   recovery_window_in_days = 7 # TODO: Lengthen?
 
   secret_string = jsonencode({
+    # used by kluctl
     "identity" = "${tls_private_key.deploy_key.private_key_openssh}"
     "known_hosts" : <<-EOKH
 github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
 github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
 github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=
 EOKH
+    # used by kargo
+    "sshPrivateKey" = "${tls_private_key.deploy_key.private_key_pem}"
+    "repoURL"       = "git@github.com:DevOps-Directive/kube-starter-kit.git"
   })
 
 }
