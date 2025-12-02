f962a18533a363684f6de4838e3371313f8c1230
“Sid <sid@devopsdirective.com>
2025-12-01 21:58:50 -0500

    update arns

--- terraform/live/shared/global/terraform-bootstrapping/outputs.tf
+++ terraform/live/shared/global/terraform-bootstrapping/outputs.tf
@@ -7,7 +7,7 @@ output "github_oidc_assume_role_arn" {
 # could create a separate role which can be assumed by the admin role (an additional hop)
 
 output "sso_admin_assume_role_arn" {
-  value = "arn:aws:iam::094905625236:role/aws-reserved/sso.amazonaws.com/us-east-2/AWSReservedSSO_AWSAdministratorAccess_fa7ea65862c3f54a"
+  value = "arn:aws:iam::094905625236:role/aws-reserved/sso.amazonaws.com/us-east-2/AWSReservedSSO_AdministratorAccess_80ecc41059967962"
 }
 
 output "tags" {
