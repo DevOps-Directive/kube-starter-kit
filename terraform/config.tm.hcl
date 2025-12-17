beff4a1ef3cf9f5a613e3ed93683c735542d27ce
“Sid <sid@devopsdirective.com>
2025-12-17 14:21:34 -0500

    fix role arns

--- terraform/config.tm.hcl
+++ terraform/config.tm.hcl
@@ -4,8 +4,8 @@ globals {
   namespace = "ksk"
 
   # GitHub OIDC role in the "infra" account (for CI/CD)
-  github_oidc_assume_role_arn = "arn:aws:iam::038198578795:role/github-oidc-provider-aws-chain"
-  sso_admin_assume_role_arn   = "arn:aws:iam::038198578795:role/AWSReservedSSO_AdministratorAccess_c43fd52097fda498"
+  github_oidc_assume_role_arn = "arn:aws:iam::094905625236:role/ksk-gbl-infra-bootstrap-github-oidc"
+  sso_admin_assume_role_arn   = "arn:aws:iam::094905625236:role/aws-reserved/sso.amazonaws.com/us-east-2/AWSReservedSSO_AdministratorAccess_80ecc41059967962"
 
   # S3 backend configuration
   backend_bucket = "ksk-gbl-infra-bootstrap-state"
--- terraform/live/staging/global/bootstrapping/main.tf
+++ terraform/live/staging/global/bootstrapping/main.tf
@@ -4,11 +4,11 @@ module "bootstrapping" {
   aws_region                  = "us-east-2"
   create_zone                 = true
   environment                 = "gbl"
-  github_oidc_assume_role_arn = "arn:aws:iam::038198578795:role/github-oidc-provider-aws-chain"
+  github_oidc_assume_role_arn = "arn:aws:iam::094905625236:role/ksk-gbl-infra-bootstrap-github-oidc"
   name                        = "bootstrap"
   namespace                   = "ksk"
   source                      = "../../../../modules//account-bootstrapping"
-  sso_admin_assume_role_arn   = "arn:aws:iam::038198578795:role/AWSReservedSSO_AdministratorAccess_c43fd52097fda498"
+  sso_admin_assume_role_arn   = "arn:aws:iam::094905625236:role/aws-reserved/sso.amazonaws.com/us-east-2/AWSReservedSSO_AdministratorAccess_80ecc41059967962"
   stage                       = "staging"
   terraform_iam_role_arn      = "arn:aws:iam::038198578795:role/ksk-gbl-staging-bootstrap-admin"
   zone_external_dns_owner     = "external-dns"
